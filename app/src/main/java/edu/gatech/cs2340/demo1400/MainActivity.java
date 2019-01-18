740ac0e4fb34585ad301e1aba626526e7a622def
rw89 <guitarbob99@icloud.com>
2019-01-18 14:10:59 -0500

    Added some attri

--- app/src/main/java/edu/gatech/cs2340/demo1400/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/demo1400/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //s,lfngsdlf
+    //sldfkgnslfkg
+    //slkfngslg
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
